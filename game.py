83e67dc0333964eed5eb1af7bdfd666a7e4110ab
Kenny Van de Maele <kenny@adimian.com>
2020-10-26 19:40:58 +0100

    initial commit
    Signed-off-by: Kenny Van de Maele <kenny@adimian.com>

---
+++ game.py
@@ -0,0 +1,197 @@
+import arcade
+import player
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "R&D Game"
+
+TILE_SCALING = 1
+
+MOVEMENT_SPEED = 4
+
+#Facing constants
+DOWN_FACING = 0
+UP_FACING = 1
+LEFT_FACING = 2
+RIGHT_FACING = 3
+
+class IntroView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+        arcade.set_background_color(arcade.color.BLACK)
+        #self.texture = arcade.load_texture("game_over.png")
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+        arcade.set_viewport(0, SCREEN_WIDTH - 1, 0, SCREEN_HEIGHT - 1)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("R&D Game",
+                         0, SCREEN_HEIGHT - 100, arcade.color.WHITE, 32, SCREEN_WIDTH, align="center")
+        arcade.draw_text("You're a classic nerd which has been transported to an anime world.\nWith this sudden teleportation come sudden powers. Try to survive!\n\nPress any Key to start.",
+                         0, SCREEN_HEIGHT - 300, arcade.color.WHITE, 16, SCREEN_WIDTH, align="center")
+
+
+    def on_key_press(self, key, modifiers):
+        """ If the user presses the mouse button, re-start the game. """
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+
+
+class GameView(arcade.View):
+    """ Main application class. """
+
+    def __init__(self):
+        super().__init__()
+
+        arcade.set_background_color(arcade.color.AMAZON)
+        # Keep track of held keys
+        self.key_up = False
+        self.key_down = False
+        self.key_left = False
+        self.key_right = False
+
+        # viewport offset
+        self.view_left = 500
+        self.view_bottom = 700
+
+        # Speed of the world moving around the player
+        self.move_x = 0
+        self.move_y = 0
+
+        # Map Layers
+        self.ground_layers = arcade.SpriteList()
+        self.colission_layers = arcade.SpriteList()
+        self.top_layers = arcade.SpriteList()
+
+        self.player = None
+        self.physics_engine = None
+
+
+    def setup(self):
+        self.player = player.PlayerCharacter(SCREEN_WIDTH, SCREEN_HEIGHT, self.view_left, self.view_bottom)
+        # Name of map file to load
+        map_name = "pipoya/SampleMap/samplemap.tmx"
+
+        # Read in the tiled map
+        overworld = arcade.tilemap.read_tmx(map_name)
+        ground_list = ["ground",]
+        colission_list = ["farm", "water", "water_grass", "building"]
+        top_list = ["farm_up", "building_up", "tree"]
+
+        for layer in ground_list:
+            self.ground_layers.extend(arcade.tilemap.process_layer(map_object=overworld,
+                                                      layer_name=layer,
+                                                      scaling=TILE_SCALING))
+
+        for layer in colission_list:
+            self.colission_layers.extend(arcade.tilemap.process_layer(map_object=overworld,
+                                                      layer_name=layer,
+                                                      scaling=TILE_SCALING,
+                                                      use_spatial_hash=True))
+        for layer in top_list:
+            self.top_layers.extend(arcade.tilemap.process_layer(map_object=overworld,
+                                                      layer_name=layer,
+                                                      scaling=TILE_SCALING))
+
+        arcade.set_viewport(self.view_left,
+                            SCREEN_WIDTH + self.view_left - 1,
+                            self.view_bottom,
+                            SCREEN_HEIGHT + self.view_bottom - 1)
+        
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.colission_layers)
+
+
+    # Check if any arrow key is currently held down
+    def iskeyheld(self):
+        return self.key_up or self.key_down or self.key_left or self.key_right
+
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP:
+            self.key_up = True
+            self.player.player_moving = True
+            self.player.character_face_direction = UP_FACING
+            self.move_y=-MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.key_down = True
+            self.player.player_moving = True
+            self.player.character_face_direction = DOWN_FACING
+            self.move_y=MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.key_left = True
+            self.player.player_moving = True
+            self.player.character_face_direction = LEFT_FACING
+            self.move_x=MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.key_right = True
+            self.player.player_moving = True
+            self.player.character_face_direction = RIGHT_FACING
+            self.move_x=-MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.UP:
+            self.key_up = False
+            self.move_y=0
+            if not self.iskeyheld():
+                self.player.player_moving = False
+        elif key == arcade.key.DOWN:
+            self.move_y=0
+            self.key_down = False
+            if not self.iskeyheld():
+                self.player.player_moving = False
+        elif key == arcade.key.LEFT:
+            self.move_x=0
+            self.key_left = False
+            if not self.iskeyheld():
+                self.player.player_moving = False
+        elif key == arcade.key.RIGHT:
+            self.move_x=0
+            self.key_right = False
+            if not self.iskeyheld():
+                self.player.player_moving = False
+
+    def on_draw(self):
+        arcade.start_render()
+
+        self.ground_layers.draw()
+        self.colission_layers.draw()
+        self.player.draw()
+        self.top_layers.draw()
+
+    def update(self, delta_time):
+        """ All the logic to move, and the game logic goes here. """
+        self.player.update()
+        self.player.update_animation()
+
+        # Center character
+        if self.player.player_moving:
+            self.player.center_x -= self.move_x
+            self.player.center_y -= self.move_y
+
+        if self.player.collides_with_list(self.colission_layers):
+            self.physics_engine.update()
+            return
+
+        self.view_left -= self.move_x
+        self.view_bottom -= self.move_y
+
+        arcade.set_viewport(self.view_left,
+                            SCREEN_WIDTH + self.view_left,
+                            self.view_bottom,
+                            SCREEN_HEIGHT + self.view_bottom)
+
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.set_vsync(True)
+    start_view = IntroView()
+    window.show_view(start_view)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,176 @@
+import arcade
+
+MAIN_PATH="images/sprites/"
+CHARACTER_SCALING = 1
+CHARACTER_RESOLUTION = 32
+UPDATES_PER_FRAME = 10
+
+#Facing constants
+DOWN_FACING = 0
+UP_FACING = 1
+LEFT_FACING = 2
+RIGHT_FACING = 3
+
+#Character constants
+NERD = 0
+OLD_MAN = 1
+
+def load_character_textures(filename, character, facing):
+    if facing == DOWN_FACING:
+        return [
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 0,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 3,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 0,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 7,
+                                y=CHARACTER_RESOLUTION * (character * 2),
+				flipped_horizontally=False)
+        ]
+    if facing == UP_FACING:
+        return [
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 1,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 4,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 1,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 4,
+                                y=CHARACTER_RESOLUTION * (character * 2),
+				flipped_horizontally=True)
+        ]
+    if facing == LEFT_FACING:
+        return [
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 2,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 5,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 2,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 6,
+                                y=CHARACTER_RESOLUTION * (character * 2)),
+        ]
+    if facing == RIGHT_FACING:
+        return [
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 2,
+                                y=CHARACTER_RESOLUTION * (character * 2),
+				flipped_horizontally=True),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 5,
+                                y=CHARACTER_RESOLUTION * (character * 2),
+				flipped_horizontally=True),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 2,
+                                y=CHARACTER_RESOLUTION * (character * 2),
+				flipped_horizontally=True),
+            arcade.load_texture(file_name=filename,
+			 	width=CHARACTER_RESOLUTION,
+				height=CHARACTER_RESOLUTION * 2,
+				x=CHARACTER_RESOLUTION * 6,
+                                y=CHARACTER_RESOLUTION * (character * 2),
+				flipped_horizontally=True)
+        ]
+
+
+class PlayerCharacter(arcade.Sprite):
+    def __init__(self, width, height, view_left, view_bottom):
+
+        # Set up parent class
+        super().__init__()
+
+        # Center character
+        self.center_x = width / 2 + view_left
+        self.center_y = height / 2 + view_bottom
+
+        # Default to face-down
+        self.character_face_direction = DOWN_FACING
+        self.player_moving = False
+
+        # Select character model
+        self.character = NERD
+
+        # Used for flipping between image sequences
+        self.cur_texture = 1
+
+        self.scale = CHARACTER_SCALING
+
+        # Adjust the collision box. Default includes too much empty space
+        # side-to-side. Box is centered at sprite center, (0, 0)
+        #self.points = [[-22, -64], [22, -64], [22, 28], [-22, 28]]
+
+        # --- Load Textures ---
+
+        self.textures_down   = load_character_textures(f"{MAIN_PATH}/spritemap.png", self.character, DOWN_FACING)
+        self.textures_up     = load_character_textures(f"{MAIN_PATH}/spritemap.png", self.character, UP_FACING)
+        self.textures_left   = load_character_textures(f"{MAIN_PATH}/spritemap.png", self.character, LEFT_FACING)
+        self.textures_right  = load_character_textures(f"{MAIN_PATH}/spritemap.png", self.character, RIGHT_FACING)
+
+        self.facingdict = {
+            DOWN_FACING: self.textures_down,
+            UP_FACING: self.textures_up,
+            LEFT_FACING: self.textures_left,
+            RIGHT_FACING: self.textures_right,
+        }
+
+        # Default texture
+        self.texture = self.textures_down[0]
+
+    def update_animation(self, delta_time: float = 1/60):
+        # Idle animation
+        if not self.player_moving:
+            self.texture = self.facingdict.get(self.character_face_direction)[0]
+            return
+
+        frame = self.cur_texture // UPDATES_PER_FRAME
+        self.texture = self.facingdict.get(self.character_face_direction)[frame]
+
+        # Walking animation
+        framecount = 4 * UPDATES_PER_FRAME - 1
+        if self.cur_texture >= framecount:
+            self.cur_texture = 0
+
+        self.cur_texture += 1
